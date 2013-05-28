39b78492d7a16c17b2633c16ebe8e386c87fc17c
Michiel Hendriks <elmuerte@users.noreply.github.com>
2013-05-28 09:54:27 +0000

    - added BROKER_NAME property to allow different broker names - support for ActiveMQ 5.8 naming scheme (ticket #1)

--- munin-activemq/src/main/java/com/mpobjects/munin/activemq/jmx/AmqJmxQuery.java
+++ munin-activemq/src/main/java/com/mpobjects/munin/activemq/jmx/AmqJmxQuery.java
@@ -18,6 +18,7 @@ package com.mpobjects.munin.activemq.jmx;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,10 +40,11 @@ public class AmqJmxQuery extends JmxConnection {
 	 */
 	protected static final Pattern DEST_PATTERN = Pattern.compile("((\\w+):)?(.*)");
 
-	private static final String KEY_TYPE = "Type";
-	private static final String KEY_DESTINATION = "Destination";
-
 	protected QueryMode mode = null;
+
+	protected NamingScheme namingScheme;
+
+	protected String brokerName = "localhost";
 
 	public AmqJmxQuery(Output aOutput) {
 		super(aOutput);
@@ -59,98 +61,63 @@ public class AmqJmxQuery extends JmxConnection {
 		mode = aMode;
 	}
 
+	@Override
+	public boolean connect() {
+		boolean ret = super.connect();
+		if (ret && namingScheme == null) {
+			determineNamingScheme();
+			if (namingScheme == null) {
+				output.err.println("Unable to determine JMX naming scheme. No ActiveMQ registered?");
+				return false;
+			}
+		}
+		return ret;
+	}
+
 	/**
-	 * Print the values for the given destinations
+	 * Print the autoconf output.
 	 * 
 	 * @param aDests
+	 * 
+	 * @param listDests
 	 */
-	public void printValues(List<String> aDests) {
-		if (mode == null) {
-			throw new IllegalStateException("No query mode was set");
-		}
+	public void printAutoConf(List<String> aDests) {
 		if (!connect()) {
-			output.setExitCode(1);
+			output.out.println("no (unable to connect)");
 			return;
 		}
-		for (String destStr : aDests) {
-			ObjectName dest;
-			try {
-				dest = getObjectName(destStr);
-			} catch (IllegalArgumentException e) {
-				output.err.println("Invalid destination: " + destStr);
-				e.printStackTrace(output.err);
-				continue;
-			}
-			printDestinationValue(dest);
-		}
-	}
-
-	/**
-	 * Print the values for a givend destination
-	 * 
-	 * @param aDest
-	 */
-	protected void printDestinationValue(ObjectName aDest) {
-		String[] attributes = getAttributeNames();
-		try {
-			AttributeList values = connection.getAttributes(aDest, attributes);
-			for (Attribute attr : values.asList()) {
-				if (attr.getValue() instanceof Number) {
-					printValue(aDest, attr.getName(), attr.getValue().toString());
-				} else {
-					output.err.println("Returned value is not a number: " + attr.toString());
-					printValue(aDest, attr.getName(), "U");
+		if (aDests.isEmpty()) {
+			output.out.println("yes (no destinations checked)");
+		} else {
+			List<String> failed = new ArrayList<String>(aDests);
+			for (String destStr : aDests) {
+				ObjectName dest;
+				try {
+					dest = getObjectName(destStr);
+				} catch (IllegalArgumentException e) {
+					output.err.println("Invalid destination specification: " + destStr);
+					continue;
 				}
+				try {
+					if (!connection.isRegistered(dest)) {
+						continue;
+					}
+				} catch (Exception e) {
+					e.printStackTrace(output.err);
+					continue;
+				}
+				failed.remove(destStr);
 			}
-		} catch (Exception e) {
-			e.printStackTrace(output.err);
-			for (String attr : attributes) {
-				printValue(aDest, attr, "U");
+			if (failed.isEmpty()) {
+				output.out.println("yes");
+			} else {
+				output.out.println("no (failed destinations:");
+				for (String destStr : failed) {
+					output.out.println(destStr);
+				}
+				output.out.println(")");
 			}
 		}
-	}
-
-	/**
-	 * Get the attribute names for a given mode
-	 * 
-	 * @param attributes
-	 * @return
-	 */
-	protected String[] getAttributeNames() {
-		switch (mode) {
-			case SIZE:
-				return new String[] { "QueueSize" };
-			case SUBSCRIBERS:
-				return new String[] { "ConsumerCount", "ProducerCount" };
-			case TRAFFIC:
-				return new String[] { "EnqueueCount", "DequeueCount" };
-			default:
-				throw new IllegalStateException("Unknown mode: " + mode);
-		}
-	}
-
-	/**
-	 * @param aDest
-	 * @param aAttr
-	 * @param aString
-	 */
-	protected void printValue(ObjectName aDest, String aAttr, String aValue) {
-		output.out.print(formatGraphName(aDest, aAttr));
-		output.out.print(".value ");
-		output.out.println(aValue);
-	}
-
-	/**
-	 * @param aDest
-	 * @param aAttr
-	 * @return
-	 */
-	protected String formatGraphName(ObjectName aDest, String aAttr) {
-		return String.format("%s_%s_%s", aDest.getKeyProperty(KEY_TYPE), aDest.getKeyProperty(KEY_DESTINATION).replaceAll("[^a-zA-Z0-9]+", "_"), aAttr);
-	}
-
-	protected String formatDestination(ObjectName aDest) {
-		return String.format("%s: %s", aDest.getKeyProperty(KEY_TYPE), aDest.getKeyProperty(KEY_DESTINATION));
 	}
 
 	/**
@@ -184,6 +151,166 @@ public class AmqJmxQuery extends JmxConnection {
 				break;
 			default:
 				throw new IllegalStateException("Unknown mode: " + mode);
+		}
+	}
+
+	public void printDestinations() {
+		if (!connect()) {
+			return;
+		}
+
+		// now try to find all queue and topics
+		try {
+			ObjectName name = ObjectName.getInstance(namingScheme.brokerBean(brokerName));
+
+			AttributeList values = connection.getAttributes(name, new String[] { "Queues", "Topics" });
+			for (Attribute attr : values.asList()) {
+				// should return an array of object names
+				if (attr.getValue() instanceof ObjectName[]) {
+					for (ObjectName objName : (ObjectName[]) attr.getValue()) {
+						String type = objName.getKeyProperty(namingScheme.destinationType());
+						String destination = objName.getKeyProperty(namingScheme.destinationName());
+						if ("queue".equalsIgnoreCase(type) || "topic".equalsIgnoreCase(type)) {
+							output.out.print(type);
+							output.out.print(":");
+							output.out.println(destination);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace(output.err);
+		}
+	}
+
+	/**
+	 * Print the values for the given destinations
+	 * 
+	 * @param aDests
+	 */
+	public void printValues(List<String> aDests) {
+		if (mode == null) {
+			throw new IllegalStateException("No query mode was set");
+		}
+		if (!connect()) {
+			output.setExitCode(1);
+			return;
+		}
+		for (String destStr : aDests) {
+			ObjectName dest;
+			try {
+				dest = getObjectName(destStr);
+			} catch (IllegalArgumentException e) {
+				output.err.println("Invalid destination: " + destStr);
+				e.printStackTrace(output.err);
+				continue;
+			}
+			printDestinationValue(dest);
+		}
+	}
+
+	/**
+	 * Try to figure out the naming scheme to use.
+	 */
+	protected void determineNamingScheme() {
+		try {
+			ObjectName name = ObjectName.getInstance(NamingScheme.INSTANCE.brokerBean(brokerName));
+
+			if (connection.isRegistered(name)) {
+				namingScheme = NamingScheme.INSTANCE;
+				return;
+			}
+
+			name = ObjectName.getInstance(NamingScheme58.INSTANCE.brokerBean(brokerName));
+			if (connection.isRegistered(name)) {
+				namingScheme = NamingScheme58.INSTANCE;
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace(output.err);
+		}
+	}
+
+	protected String formatDestination(ObjectName aDest) {
+		return String.format("%s: %s", aDest.getKeyProperty(namingScheme.destinationType()), aDest.getKeyProperty(namingScheme.destinationName()));
+	}
+
+	/**
+	 * @param aDest
+	 * @param aAttr
+	 * @return
+	 */
+	protected String formatGraphName(ObjectName aDest, String aAttr) {
+		return String.format("%s_%s_%s", aDest.getKeyProperty(namingScheme.destinationType()), aDest.getKeyProperty(namingScheme.destinationName()).replaceAll("[^a-zA-Z0-9]+", "_"), aAttr);
+	}
+
+	/**
+	 * Get the attribute names for a given mode
+	 * 
+	 * @param attributes
+	 * @return
+	 */
+	protected String[] getAttributeNames() {
+		switch (mode) {
+			case SIZE:
+				return new String[] { "QueueSize" };
+			case SUBSCRIBERS:
+				return new String[] { "ConsumerCount", "ProducerCount" };
+			case TRAFFIC:
+				return new String[] { "EnqueueCount", "DequeueCount" };
+			default:
+				throw new IllegalStateException("Unknown mode: " + mode);
+		}
+	}
+
+	/**
+	 * Get the object name for a given destination
+	 * 
+	 * @param destination
+	 *            The destination.
+	 * @return
+	 */
+	protected ObjectName getObjectName(String destination) {
+		String type = "Queue";
+		String name = destination;
+
+		Matcher matcher = DEST_PATTERN.matcher(destination);
+		if (!matcher.matches()) {
+			throw new IllegalArgumentException(String.format("Invalid destination: %s", destination));
+		}
+
+		if (matcher.group(2) != null) {
+			type = matcher.group(2);
+		}
+		name = matcher.group(3).trim();
+
+		if ("queue".equalsIgnoreCase(type)) {
+			type = "Queue";
+		} else if ("topic".equalsIgnoreCase(type)) {
+			type = "Topic";
+		} else {
+			throw new IllegalArgumentException(String.format("Invalid type '%s' in destination: %s", type, destination));
+		}
+
+		if (name == null || name.length() == 0) {
+			throw new IllegalArgumentException(String.format("No destination name given: %s", destination));
+		}
+
+		try {
+			return ObjectName.getInstance(namingScheme.destinationBean(brokerName, type, name));
+		} catch (MalformedObjectNameException e) {
+			throw new IllegalArgumentException("Unable to create object name for destination: " + destination, e);
+		}
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		Map<String, String> env = System.getenv();
+		if (env.get("BROKER_NAME") != null) {
+			brokerName = env.get("BROKER_NAME");
+		} else {
+			brokerName = System.getProperty("BROKER_NAME", brokerName);
 		}
 	}
 
@@ -256,116 +383,27 @@ public class AmqJmxQuery extends JmxConnection {
 	}
 
 	/**
-	 * Print the autoconf output.
+	 * Print the values for a givend destination
 	 * 
-	 * @param aDests
-	 * 
-	 * @param listDests
+	 * @param aDest
 	 */
-	public void printAutoConf(List<String> aDests) {
-		if (!connect()) {
-			output.out.println("no (unable to connect)");
-			return;
-		}
-		if (aDests.isEmpty()) {
-			output.out.println("yes (no destinations checked)");
-		} else {
-			List<String> failed = new ArrayList<String>(aDests);
-			for (String destStr : aDests) {
-				ObjectName dest;
-				try {
-					dest = getObjectName(destStr);
-				} catch (IllegalArgumentException e) {
-					output.err.println("Invalid destination specification: " + destStr);
-					continue;
-				}
-				try {
-					if (!connection.isRegistered(dest)) {
-						continue;
-					}
-				} catch (Exception e) {
-					e.printStackTrace(output.err);
-					continue;
-				}
-				failed.remove(destStr);
-			}
-			if (failed.isEmpty()) {
-				output.out.println("yes");
-			} else {
-				output.out.println("no (failed destinations:");
-				for (String destStr : failed) {
-					output.out.println(destStr);
-				}
-				output.out.println(")");
-			}
-		}
-	}
-
-	public void printDestinations() {
-		if (!connect()) {
-			return;
-		}
-
-		// no try to find all queue and topics
+	protected void printDestinationValue(ObjectName aDest) {
+		String[] attributes = getAttributeNames();
 		try {
-			AttributeList values = connection.getAttributes(ObjectName.getInstance("org.apache.activemq:BrokerName=localhost,Type=Broker"), new String[] { "Queues", "Topics" });
+			AttributeList values = connection.getAttributes(aDest, attributes);
 			for (Attribute attr : values.asList()) {
-				// should return an array of object names
-				if (attr.getValue() instanceof ObjectName[]) {
-					for (ObjectName objName : (ObjectName[]) attr.getValue()) {
-						String type = objName.getKeyProperty(KEY_TYPE);
-						String destination = objName.getKeyProperty(KEY_DESTINATION);
-						if ("queue".equalsIgnoreCase(type) || "topic".equalsIgnoreCase(type)) {
-							output.out.print(type);
-							output.out.print(":");
-							output.out.println(destination);
-						}
-					}
+				if (attr.getValue() instanceof Number) {
+					printValue(aDest, attr.getName(), attr.getValue().toString());
+				} else {
+					output.err.println("Returned value is not a number: " + attr.toString());
+					printValue(aDest, attr.getName(), "U");
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace(output.err);
-		}
-	}
-
-	/**
-	 * Get the object name for a given destination
-	 * 
-	 * @param destination
-	 *            The destination.
-	 * @return
-	 */
-	protected ObjectName getObjectName(String destination) {
-		String broker = "localhost";
-		String type = "Queue";
-		String name = destination;
-
-		Matcher matcher = DEST_PATTERN.matcher(destination);
-		if (!matcher.matches()) {
-			throw new IllegalArgumentException(String.format("Invalid destination: %s", destination));
-		}
-
-		if (matcher.group(2) != null) {
-			type = matcher.group(2);
-		}
-		name = matcher.group(3).trim();
-
-		if ("queue".equalsIgnoreCase(type)) {
-			type = "Queue";
-		} else if ("topic".equalsIgnoreCase(type)) {
-			type = "Topic";
-		} else {
-			throw new IllegalArgumentException(String.format("Invalid type '%s' in destination: %s", type, destination));
-		}
-
-		if (name == null || name.length() == 0) {
-			throw new IllegalArgumentException(String.format("No destination name given: %s", destination));
-		}
-
-		try {
-			return ObjectName.getInstance(String.format("org.apache.activemq:BrokerName=%s,Type=%s,Destination=%s", broker, type, name));
-		} catch (MalformedObjectNameException e) {
-			throw new IllegalArgumentException("Unable to create object name for destination: " + destination, e);
+			for (String attr : attributes) {
+				printValue(aDest, attr, "U");
+			}
 		}
 	}
 
@@ -376,5 +414,16 @@ public class AmqJmxQuery extends JmxConnection {
 	 */
 	protected final void println(String str) {
 		output.out.println(str);
+	}
+
+	/**
+	 * @param aDest
+	 * @param aAttr
+	 * @param aString
+	 */
+	protected void printValue(ObjectName aDest, String aAttr, String aValue) {
+		output.out.print(formatGraphName(aDest, aAttr));
+		output.out.print(".value ");
+		output.out.println(aValue);
 	}
 }
